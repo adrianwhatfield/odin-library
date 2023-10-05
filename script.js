f583988aaebc8b2c9f76b4b39d2cd7c8e4536da0
Adrian Hatfield <adrianhatfield23@gmail.com>
2023-10-05 18:29:26 +0100

    Create displayLibrary()

--- script.js
+++ script.js
@@ -17,3 +17,42 @@ function Book(title, author, pages, isRead) {
 function addBookToLibrary(book) {
     myLibrary.push(book)
 }
+
+function displayLibrary() {
+    const booksContainer = document.getElementById("books");
+
+    for (let book in myLibrary) {
+        const bookDiv = document.createElement("div");
+        bookDiv.setAttribute("class", "book");
+        booksContainer.appendChild(bookDiv);
+
+        let thisBook = myLibrary[book];
+    
+        const title = document.createElement("p");
+        title.innerHTML = thisBook.title;
+        bookDiv.appendChild(title);
+    
+        const author = document.createElement("p");
+        author.innerHTML = thisBook.author;
+        bookDiv.appendChild(author);
+
+        const pages = document.createElement("p");
+        pages.innerHTML = thisBook.pages;
+        bookDiv.appendChild(pages);
+
+        const read = document.createElement("p");
+        if (thisBook.isRead == true) {
+            read.innerHTML = "Yes";
+        } else if (thisBook.isRead == false) {
+            read.innerHTML = "No";
+        }
+        bookDiv.appendChild(read);
+    }
+}
+
+let myBook = new Book("title name", "author name", 200, true);
+addBookToLibrary(myBook);
+addBookToLibrary(myBook);
+addBookToLibrary(myBook);
+addBookToLibrary(myBook);
+displayLibrary()
